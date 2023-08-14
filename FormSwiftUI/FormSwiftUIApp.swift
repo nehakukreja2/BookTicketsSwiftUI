809a6aea6a29ee78cfc1e597e4b37ceacf62f9b3
Neha Kukreja <neha.kukreja@appinventiv.com>
2023-08-14 22:04:16 +0530

    Initial Commit

---
+++ FormSwiftUI/FormSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormSwiftUIApp.swift
+//  FormSwiftUI
+//
+//  Created by Neha Kukreja on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FormSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
